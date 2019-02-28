0ab3e7ec72874a896b0e74f31078885dc7e083ed
HU Xiao <hux062303@gmail.com>
2019-02-28 08:52:34 +0100

    almost done

--- Week 02 - Scale-space (Part I)/script.m
+++ Week 02 - Scale-space (Part I)/script.m
@@ -9,7 +9,7 @@ end
 addpath ../data/EX_2_data;
 addpath ../utils/
 
-skip = [1 3 4 5 6 7];
+skip = [1 2 4 5 6 7];
 drawGif = 0;
 
 % since we will work with second order derivation, we need this template
@@ -121,7 +121,7 @@ if isempty(find(skip == 1,1))
     title('$Multi-scale\ blob\ detection$','FontSize',fontSize,'Interpreter','latex');
 end
 
-%% ex2: computing contour length
+%% ex2: TODO
 if isempty(find(skip == 2,1))
     close all;
     imgnames = {'CT_lab_high_res.png', ...
@@ -131,7 +131,7 @@ if isempty(find(skip == 2,1))
              'Optical.png', ...
              'SEM.png'};
     % 4 5, need tuning
-    for kk = 4%size(imgnames,1) 
+    for kk = 1%size(imgnames,1) 
         I0 = imread(imgnames{kk});
         I0 = im2double(I0);
         if size(I0,3) == 3
@@ -141,24 +141,29 @@ if isempty(find(skip == 2,1))
         end
 %         Igray = imresize(Igray,[480,640]);
         [M,N] = size(Igray);
-        K = 2;
-        t0 = 2;
+        K = 6;
+        t0 = 2^(1/2);
         [LLNs, radius] = create_scale_normalized_LoG(Igray, t0, K);
 %         half_win_size = 1;
 %         blobs = detect_blobs(LLNs, half_win_size);
-        for i = 1:1:K
+        LLMax = max(abs(LLNs),[],3);
+
+        blobs = [];
+        for i = 1:1:1
             half_win_size = 1;
-            [blobs_center] = detect_blobs(LLNs(:,:,i), half_win_size);
-            blobs = [blobs_center repmat(radius(i),size(blobs_center,1),1)];
+            [blobs_center] = detect_blobs(LLMax, half_win_size);
+            blobs = [blobs;blobs_center repmat(radius(i),size(blobs_center,1),1)];
         end
         %% drawing
         Ic2 = cat(3,Igray,Igray,Igray);
         w = linspace(0,2*pi,100);
         dtured = [153/255 0 0];
         for i = 1:size(blobs,1)
-            radius = blobs(i,3);
-            rcw = radius*cos(w);
-            rsw = radius*sin(w);
+            val = LLNs(blobs(i,1),blobs(i,2),:);
+            [~,maxid] = max(abs(val));
+            r = radius(maxid);
+            rcw = r*cos(w);
+            rsw = r*sin(w);
             xx = round(blobs(i,1) + rcw);
             yy = round(blobs(i,2) + rsw);
             valid = xx > 0 & xx <= M & yy > 0 & yy <= N;
@@ -179,7 +184,7 @@ end
 if isempty(find(skip == 3,1))
     close all;
     imgnames = {'CT_lab_high_res.png', ...
-             'CT_lab_low_res.png', ...
+             'CT_lab_med_res.png', ...
              'CT_lab_low_res.png', ...
              'CT_synchrotron.png', ...
              'Optical.png', ...
@@ -196,10 +201,9 @@ if isempty(find(skip == 3,1))
 %         Igray = imresize(Igray,[480,640]);
         [M,N] = size(Igray);
         
-        t = sqrt(2);%1
+        t = 4;%1 4 2 3 3 1 4 4, 5 4,6 4
         sigma = 3;
         g = gassian_fast(t, sigma);
-        
         Iblur = imfilter(Igray, g, 'replicate');
         Iblur = imfilter(Iblur, g', 'replicate');
         
@@ -207,46 +211,45 @@ if isempty(find(skip == 3,1))
 %         Ib = imbinarize(Iblur, threshold);
         imshow(Iblur,[]);
         
-        half_win_size = 3;
+        half_win_size = 1;
         [local_maxima, Idebug] = find_maxima(Iblur, half_win_size);
         figure
         imshow(Idebug,[]);
         
         [M,N] = size(Igray);
-        K = 8;
-        t0 = 1;
-        LLNs = zeros(M,N,K);
-        scales = zeros(1,K);
-        for k = 1:K
-            t = sqrt(2)^(k-1)*t0;
-            [LLN] = scale_normalized_LoG(Igray, t);
-            LLNs(:,:,k) = LLN;
-            scales(k) = t;
-        end
-        
+        K = 10;
+        t0 = 2^(1/5);
+        [LLNs, radius] = create_scale_normalized_LoG(Igray, t0, K);
+%         blobs = [];
+%         for i = 1:1:K
+%             half_win_size = 1;
+%             [blobs_center] = detect_blobs(LLNs(:,:,i), half_win_size);
+%             blobs = [blobs_center repmat(radius(i),size(blobs_center,1),1)];
+%         end
+        LLMax = max(abs(LLNs),[],3);
+        %% drawing
         Ic2 = cat(3,Igray,Igray,Igray);
-        ccmap = jet(128);
         w = linspace(0,2*pi,100);
         dtured = [153/255 0 0];
-        
-        for k = 1:size(local_maxima,1)
-            val = LLNs(local_maxima(k,1),local_maxima(k,2),:);
-            [~,minid] = min(val);
-            scale = scales(minid);
-            radius = sqrt(2)*scale;
-            rcw = radius*cos(w);
-            rsw = radius*sin(w);
-            xx = round(local_maxima(k,1) + rcw);
-            yy = round(local_maxima(k,2) + rsw);
+        for i = 1:size(local_maxima,1)
+            val = LLNs(local_maxima(i,1),local_maxima(i,2),:);
+            [~,maxid] = max(abs(val));
+%             maxid
+            r = radius(maxid);
+            rcw = r*cos(w);
+            rsw = r*sin(w);
+            xx = round(local_maxima(i,1) + rcw);
+            yy = round(local_maxima(i,2) + rsw);
             valid = xx > 0 & xx <= M & yy > 0 & yy <= N;
             indices = sub2ind([M,N], xx(valid), yy(valid));
-            nn = [-M +M -1 +1];
-            indices = [indices indices+nn(1) indices+nn(2) indices+nn(3) indices+nn(4)];
+%             nn = [-M +M -1 +1];
+%             indices = [indices indices+nn(1) indices+nn(2) indices+nn(3) indices+nn(4)];
             indices = indices(indices > 0 & indices < M*N);
             Ic2(indices) = dtured(1);%ccmap(i,1);
             Ic2(indices+M*N) = dtured(2);%ccmap(i,2);
             Ic2(indices+M*N*2) = dtured(3);%ccmap(i,3);
         end
+        
         figure
         imshow(Ic2,[]);
         title('$Multi-scale\ blob\ detection$','FontSize',fontSize,'Interpreter','latex');
@@ -267,28 +270,39 @@ function res = secondorder_der_gaussian_fast(t, sigma)
 end
 
 function varargout = create_scale_normalized_LoG(Igray, t0, K)
+%     tsqrt = t0;% prev scale
+%     prevLayer = Igray;
+%     LLs = zeros(size(Igray,1),size(Igray,2),K);
+%     radius = zeros(1,K);
+% 
+%     sigma = 3;
+%     g = gassian_fast(tsqrt, sigma);
+%     dgg = secondorder_der_gaussian_fast(tsqrt, sigma);
+%     
+%     for k = 1:K
+%         % do layer from the previsou layer
+%         % gaussian filter
+%         Ixx = imfilter(prevLayer, dgg, 'replicate');% along x
+%         Ixx = imfilter(Ixx, g', 'replicate');% smooth y, since g=gx*gy, dg=dgx gy + gx dgy. partial dev only pick one item
+%         Iyy = imfilter(prevLayer, dgg', 'replicate');% along y
+%         Iyy = imfilter(Iyy, g, 'replicate');%along x
+%         LL = Ixx+Iyy;
+%         prevLayer = LL;
+%         LL = LL * (tsqrt.^2);
+%         LLs(:,:,k) = LL;
+%         tsqrt = tsqrt * sqrt(2); % scale update
+%         radius(k) = tsqrt;
+%     end
     tsqrt = t0;% prev scale
-    prevLayer = Igray;
     LLs = zeros(size(Igray,1),size(Igray,2),K);
     radius = zeros(1,K);
-
-    sigma = 3;
-    g = gassian_fast(tsqrt, sigma);
-    dgg = secondorder_der_gaussian_fast(tsqrt, sigma);
-    
     for k = 1:K
-        % do layer from the previsou layer
-        % gaussian filter
-        Ixx = imfilter(prevLayer, dgg, 'replicate');% along x
-        Ixx = imfilter(Ixx, g', 'replicate');% smooth y, since g=gx*gy, dg=dgx gy + gx dgy. partial dev only pick one item
-        Iyy = imfilter(prevLayer, dgg', 'replicate');% along y
-        Iyy = imfilter(Iyy, g, 'replicate');%along x
-        LL = Ixx+Iyy;
-        LL = LL * tsqrt.^2;
+        [LL] = scale_normalized_LoG(Igray, tsqrt);
         LLs(:,:,k) = LL;
         tsqrt = tsqrt * sqrt(2); % scale update
         radius(k) = tsqrt;
     end
+
     varargout{1} = LLs;
     varargout{2} = radius;
 end
@@ -309,101 +323,53 @@ function [LL] = scale_normalized_LoG(I, tsqrt)
 end
 
 function [blobs_center] = detect_blobs(I, half_win_size)
-    [xx,yy]=meshgrid([-half_win_size:1:half_win_size], ...
-                     [-half_win_size:1:half_win_size]);
-    xx = xx(:);
-    yy = yy(:);
-    
-    idc = xx == 0 & yy == 0;
-    xx(idc) = []; yy(idc) = [];
-    
-    [M,N] = size(I);
-    
-    maxcc = zeros(M*N,2);
-    k1 = 0;
-    
+%     I = abs(I);
+%     thresh = 0.2*max(I(:));
     I = abs(I);
-    maxval = max(I(:));
-    I(I<0.3*maxval) = 0;
-    for i = 1:M
-        subx = xx + i;
-        for j = 1:N
-            suby = yy + j;
-            valid = subx>0 & subx < M & suby>0 & suby < N;
-            validx = subx(valid);validy = suby(valid);
-            indxy = sub2ind([M,N],validx,validy);
-%             indxy = indxy(indxy > 0 & indxy <= M*N);
-            maxval = max(I(indxy));
-            if I(i,j) > maxval
-                % maxima
-                k1 = k1+1;
-                maxcc(k1,:) = [i,j];
-            end
-        end
-    end
-    maxcc(k1+1:end,:) = [];
-    blobs_center = [maxcc];
+    val = I(:);
+    meanval = mean(val);
+    stdv = var(val);
+    thresh = meanval+1*stdv;%0.3*max(I(:));
+    mx = imdilate(I, strel('square',2*half_win_size+1));
+
+    % Make mask to exclude points within radius of the image boundary. 
+    bordermask = zeros(size(I));
+    bordermask(half_win_size+1:end-half_win_size, half_win_size+1:end-half_win_size) = 1;
+    
+    % Find maxima, threshold, and apply bordermask
+    cimmx = (I==mx) & (I>thresh) & bordermask;
+    [r,c] = find(cimmx);        % Find row,col coords.
+    blobs_center = [r c];
 end
 
 function [varargout] = find_maxima(I, half_win_size)
-    [xx,yy]=meshgrid([-half_win_size:1:-1,1:1:half_win_size], ...
-                     [-half_win_size:1:-1,1:1:half_win_size]);
-    xx = xx(:);
-    yy = yy(:);
-    [M,N] = size(I);
-    
-    maxcc = zeros(M*N,2);
-    mincc = zeros(M*N,2);
-    k1 = 0;
-    k2 = 0;
-    
-    for i = 1:M
-        subx = xx + i;
-        for j = 1:N
-            suby = yy + j;
-            valid = subx>0 & subx < M & suby>0 & suby < N;
-            validx = subx(valid);validy = suby(valid);
-            indxy = sub2ind([M,N],validx,validy);
-%             indxy = indxy(indxy > 0 & indxy <= M*N);
-            maxval = max(I(indxy));
-            if I(i,j) > maxval
-                % maxima
-                k1 = k1+1;
-                maxcc(k1,:) = [i,j];
-%             else
-%                 minval = min(I(indxy));
-%                 if I(i,j) < minval
-%                     % minima
-%                     k2 = k2 + 1;
-%                     mincc(k2,:) = [i,j];
-%                 end
-            end
-        end
-    end
-    maxcc(k1+1:end,:) = [];
-    mincc(k2+1:end,:) = [];
-    
-    % further refinement
-    indices = sub2ind(size(I), maxcc(:,1), maxcc(:,2));
-    val = I(indices);
+%     I = abs(I);
+    val = I(:);
     meanval = mean(val);
     stdv = var(val);
-    maxcc = maxcc(abs(val - meanval) < 5*stdv, :);
+    thresh = meanval+2*stdv;%0.3*max(I(:));
+    mx = imdilate(I, strel('square',2*half_win_size+1));
+
+    % Make mask to exclude points within radius of the image boundary. 
+    bordermask = zeros(size(I));
+    bordermask(half_win_size+1:end-half_win_size, half_win_size+1:end-half_win_size) = 1;
     
-    blobs_center = maxcc;
-%     % visulization
-    cc = [maxcc];
+    % Find maxima, threshold, and apply bordermask
+    cimmx = (I==mx) & (I>thresh) & bordermask;
+    [r,c] = find(cimmx);        % Find row,col coords.
+    blobs_center = [r c];
+    
+    % visulization
+    cc = blobs_center;
     num_segments = size(cc, 1);
-    ccmap = jet(num_segments);
     [M,N] = size(I);
     Idebug = cat(3,I,I,I);
     for i = 1:num_segments
         indices = sub2ind(size(I), cc(i,1), cc(i,2));
-        Idebug(indices) = 1;%ccmap(i,1);
-        Idebug(indices+M*N) = 0;%ccmap(i,2);
-        Idebug(indices+M*N*2) = 0;%ccmap(i,3);
+        Idebug(indices) = 1;
+        Idebug(indices+M*N) = 0;
+        Idebug(indices+M*N*2) = 0;
     end
-%     imshow(Ic,[]);
     varargout{1} = blobs_center;
     if nargout == 2
         varargout{2} = Idebug;
