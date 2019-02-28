f571db3b9a79a5dd1585852c8ea144bfb39d4f24
HU Xiao <hux062303@gmail.com>
2019-02-28 21:17:38 +0100

    finalize

--- test/test_fast_dop_computation.m
+++ test/test_fast_dop_computation.m
@@ -45,7 +45,7 @@ function test_fast_dop_computation
         
         %% 4
         tic;
-        dop4(k) = eig_decomp2(H);
+        dop4(k) = trial(H);
         t4s(k) = toc;
  
         %% 5
@@ -119,9 +119,79 @@ function dop = eig_decomp2(H)
 
 end
 
+function [p3,p4] = coeff3(M)
+    M12_M21=M(1,2)*M(2,1);
+    M11_M22=M(1,1)*M(2,2);
+    M13_M31=M(1,3)*M(3,1);
+    M23_M32=M(2,3)*M(3,2);
+    
+    % M1_2*M2_1 - M1_1*M2_2 - M1_1*M3_3 + M1_3*M3_1 - M2_2*M3_3 + M2_3*M3_2, 
+    p3 = M12_M21 - M11_M22 - M(1,1)*M(3,3) + M13_M31 - M(2,2)*M(3,3) + M23_M32;
+    
+    % M1_1*M2_2*M3_3 - M1_1*M2_3*M3_2 - M1_2*M2_1*M3_3 + M1_2*M2_3*M3_1 + M1_3*M2_1*M3_2 - M1_3*M2_2*M3_1, 
+    p4 = M11_M22*M(3,3)-M(1,1)*M23_M32-M12_M21*M(3,3)+M(1,2)*M(2,3)*M(3,1)+M(1,3)*M(2,1)*M(3,2)-M13_M31*M(2,2);
+end
+
+function p = coeff4(M)
+    n = size(M,1);
+    p = zeros(1,n+1);
+    p(1) = 1;
+    C = M;
+    p(2) = -trace(C);
+    for k = 2:n
+        C(1,1)=C(1,1)+p(k);
+        C(2,2)=C(2,2)+p(k);
+        C(3,3)=C(3,3)+p(k);
+        C = M*C;
+        p(k+1)=-trace(C)/k;
+    end
+end
+
+function dop = trial(H)
+    M = H'*H;
+    p1 = coeff4(M);
+    S1 = M(1:3,1:3) - M(1:3,4)*M(4,1:3)./M(4,4);
+    p2 = coeff4(S1);
+    dop = sqrt(-p1(end-1)/p1(end));
+    pdop = sqrt(-p2(end-1)/p2(end));
+    tdop = sqrt(-p1(end-1)/p1(end)+p2(end-1)/p2(end));
+end
+
 function dop = fast_eig_decomp(H)
     M = H'*H;
 
+    %% gdop
+    a = M(1,1);b = M(1,2);c = M(1,3);d = M(1,4);
+    e = M(2,2);f = M(2,3);g = M(2,4);
+    h = M(3,3);i = M(3,4);
+    j = M(4,4);
+
+    bb = b*b;
+    cc = c*c;
+    dd = d*d;
+    ff = f*f;
+    gg = g*g;
+    ii = i*i;
+    ae = a*e;
+    eh = e*h;
+    ah = a*h;
+    aj = a*j;
+    ej = e*j;
+    hj = h*j;
+
+    fgi = f*g*i;
+    bcf = b*c*f;
+    bdg = b*d*g;
+    ehj = e*h*j;
+    
+    p4 = a*ff+ a*gg+ cc*e + dd*e+ a*ii+ bb*h + bb*j+ dd*h + cc*j+ e*ii + ...
+        gg*h+ ff*j - 2*fgi- ehj- 2*bcf- 2*bdg- ae*h- ae*j- 2*c*d*i- ah*j;
+
+    p5 = cc*gg + dd*ff + bb*ii - ae*ii - ah*gg - aj*ff - dd*eh - cc*ej - ...
+        2*c*d*f*g + 2*bcf*j - 2*b*c*g*i - 2*b*d*f*i + 2*bdg*h + 2*c*d*e*i ...
+        + 2*a*fgi - bb*hj + a*ehj;
+    dop = sqrt(-p4/p5);
+%     p = coeff4(M);
 %     A = H(:,1:3)'*H(:,1:3);
 %     s1 = sum(H(:,1));s2 = sum(H(:,2));s3 = sum(H(:,3));
 %     s1s1 = s1*s1;
@@ -131,17 +201,29 @@ function dop = fast_eig_decomp(H)
 %     s2s2 = s2*s2;
 %     s3s3 = s3*s3;
 %     N = size(H,1);
-    
-    tic
-    [e1,v1] = eig(M(1:3,1:3));
-    S1 = M(1:3,1:3) - M(1:3,4)*M(1:3,4)'./M(4,4);
-    S2 = M(4,4) - M(4,1:3)*e1*diag([1/v1(1,1),1/v1(2,2),1/v1(3,3)])*e1'*M(1:3,4);
-    e2 = eig(S1);     
-    dop = sqrt(1/e2(1)+1/e2(2)+1/e2(3) + 1/S2);
-    toc
-    tic
-    Minv = inv(M);
-    toc
+%     tic
+
+%     [e1,v1] = eig(M(1:3,1:3));
+    S1 = M(1:3,1:3) - M(1:3,4)*M(4,1:3)./M(4,4);% M(1:3,1:3) - [s1s1 s1s2 s1s3;s1s2 s2s2 s2s3;s1s3 s2s3 s3s3]./N;%
+%     tic
+%     p = coeff4(S1);
+%     ps3 = p(end-1);ps4 = p(end);
+%     toc
+%     tic
+    [ps3,ps4] = coeff3(S1);
+%     toc
+    pdop = sqrt(-ps3/ps4);
+    tdop = sqrt(-p4/p5+ps3/ps4);
+%     S2 = M(4,4) - M(4,1:3)*e1*diag([1/v1(1,1),1/v1(2,2),1/v1(3,3)])*e1'*M(1:3,4);
+%     e2 = eig(S1);     
+%     pdop = sqrt(1/e2(1)+1/e2(2)+1/e2(3));
+%     tdop = sqrt(dop*dop-pdop*pdop);
+
+%     dop = sqrt(1/e2(1)+1/e2(2)+1/e2(3) + 1/S2);
+%     toc
+%     tic
+%     Minv = inv(M);
+%     toc
 end
 
 function dop = eig_decomp(H)
@@ -159,7 +241,10 @@ function dop = dop_via_cl(H)
 end
 
 function dop = dop_via_inverse(H)
-    dop = sqrt(trace(inv(H'*H)));
+    Minv = inv(H'*H);
+    dop = sqrt(trace(Minv));
+    pdop = sqrt(trace(Minv(1:3,1:3)));
+    tdop = sqrt(trace(Minv(4,4)));
 end
 
 function random_vecs = gen_ran_vecs(n, dim)
@@ -168,5 +253,8 @@ function random_vecs = gen_ran_vecs(n, dim)
 end
 
 function nvecs = v_normalize(vecs)
-    nvecs = vecs./vecnorm(vecs,2,2);
+%     nvecs = vecs./vecnorm(vecs,2,2);
+    for i = 1:size(vecs,1)
+        nvecs(i,:) = vecs(i,:)./norm(vecs(i,:),2);
+    end
 end
