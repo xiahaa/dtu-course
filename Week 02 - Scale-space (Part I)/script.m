99daa0515844a929115615a3731025d6d6f4cc4b
HU Xiao <hux062303@gmail.com>
2019-02-28 08:53:28 +0100

    remove false normalized scale space code

--- Week 02 - Scale-space (Part I)/script.m
+++ Week 02 - Scale-space (Part I)/script.m
@@ -270,29 +270,6 @@ function res = secondorder_der_gaussian_fast(t, sigma)
 end
 
 function varargout = create_scale_normalized_LoG(Igray, t0, K)
-%     tsqrt = t0;% prev scale
-%     prevLayer = Igray;
-%     LLs = zeros(size(Igray,1),size(Igray,2),K);
-%     radius = zeros(1,K);
-% 
-%     sigma = 3;
-%     g = gassian_fast(tsqrt, sigma);
-%     dgg = secondorder_der_gaussian_fast(tsqrt, sigma);
-%     
-%     for k = 1:K
-%         % do layer from the previsou layer
-%         % gaussian filter
-%         Ixx = imfilter(prevLayer, dgg, 'replicate');% along x
-%         Ixx = imfilter(Ixx, g', 'replicate');% smooth y, since g=gx*gy, dg=dgx gy + gx dgy. partial dev only pick one item
-%         Iyy = imfilter(prevLayer, dgg', 'replicate');% along y
-%         Iyy = imfilter(Iyy, g, 'replicate');%along x
-%         LL = Ixx+Iyy;
-%         prevLayer = LL;
-%         LL = LL * (tsqrt.^2);
-%         LLs(:,:,k) = LL;
-%         tsqrt = tsqrt * sqrt(2); % scale update
-%         radius(k) = tsqrt;
-%     end
     tsqrt = t0;% prev scale
     LLs = zeros(size(Igray,1),size(Igray,2),K);
     radius = zeros(1,K);
