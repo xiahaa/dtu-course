2c118a7de7a155c19ea18f34fec934388feaf465
HU Xiao <hux062303@gmail.com>
2019-04-21 17:57:39 +0200

    augmentation

--- assignment_4/data_augmentation.m
+++ assignment_4/data_augmentation.m
@@ -14,18 +14,33 @@ test_data = single(test_data);
 test_label = single(test_label);
 
 %%
-im = reshape(test_data',28,28,1,[]);
-im = permute(im,[2,1,3,4]);
+im = reshape(train_data',28,28,1,[]);
+% im = permute(im,[2,1,3,4]);
 
-[~,labels] = max(test_label');
+[~,labels] = max(train_label');
 
+%%%%%%%%%%%%%%%%%%%%%%% injecting noise %%%%%%%%%%%%%%%%%%%%%%%%%%
+imns = [];
+lbns = [];
 for i = 1:10
     id = find(labels == i);
-    for j = 1:20:length(id)
-        imshow(im(:,:,:,id(j)));
-        pause(0.1);
+    % 
+    idn = round(length(id) * 0.8);
+    ids = id(randperm(length(id),idn));
+    
+    imn = zeros(28,28,1,length(ids)*2);
+    
+    for j = 1:1:length(ids)
+        imn(:,:,:,(j-1)*2+1) = randomNoise(im(:,:,:,ids(j)),0.3);
+        imn(:,:,:,(j-1)*2+2) = rescaling(im(:,:,:,ids(j)));
     end
+    imns = cat(4,imns,imn);
+    lb = single(zeros(10,size(imn,4)));
+    lb(i,:) = 1;
+    lbns = cat(2,lbns,lb);
 end
+imns = reshape(imns,28*28,[])';
+save('aug3.mat','imns','lbns');
 
 function imt = randomRot(im,maxdeg)
 % max 10, ok
@@ -50,6 +65,10 @@ end
 
 function imt = rescaling(im)
 % function like smoothing
-    ims = imresize(im,0.8,'bilinear');
-    imt = imresize(ims,size(im),'bilinear');
+%     ims = imresize(im,0.8,'bilinear');
+%     imt = imresize(ims,size(im),'bilinear');
+    t = [1;1]+rand([2,1]).*0.6-0.3;
+    tform = affine2d([t(1) 0 0;0 t(2) 0;0 0 1]');
+    R = imref2d(size(im));
+    imt = imwarp(im,tform,'InterpolationMethod','bilinear','OutputView',R);
 end
