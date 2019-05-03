d969ccda60e1f0f6f18948b0bff890f851941833
DTU Space drone <xiahaa@space.dtu.dk>
2019-05-03 18:24:44 +0200

    add hs dense optical flow

--- mini-project/optical_flow/script.m
+++ mini-project/optical_flow/script.m
@@ -2,7 +2,7 @@ clc;close all;clear all;
 
 addpath ../../utils;
 
-baseDir = '../../data/optical_flow_data/Basketball';
+baseDir = '../../data/optical_flow_data';
 
 % load database
 buildingScene = imageDatastore(baseDir);
@@ -15,13 +15,13 @@ im1 = imPreprocessing(im1);
 im2 = imread(buildingScene.Files{2});
 im2 = imPreprocessing(im2);
 
-hsize = 10;
+hsize = 3;
 height = size(im1,1);
 width = size(im1,2);
 
 % image size
 [flow_u, flow_v] = denseflowLK(im1, im2, hsize);
-figure;imshow(im1, 'InitialMagnification',3000);hold on;
+figure;imshow(im1, 'InitialMagnification',1000);hold on;
 
 % opflow = opticalFlow(flow_u,flow_v);
 % plot(opflow,'DecimationFactor',[1 1],'ScaleFactor',1);
@@ -34,7 +34,7 @@ quiver(xx,yy,flow_u(:),flow_v(:),'LineWidth',1.5, 'Color','r','MaxHeadSize',1);a
 % display dense flow as an image
 img = computeColor(flow_u,flow_v);
 figure
-imshow(img);hold on;
+imshow(img, 'InitialMagnification',1000);hold on;
 
 function [Ix, Iy] = grad1(im1)
 % perform the simplest gradient calculation for LK optical flow.
@@ -79,35 +79,6 @@ function It = gradIntensity(u,v,flowu,flowv,im1,im2)
     It = im2(index1) - im1(index0);
 end
 
-function g = gaussian_kernel_calculator(D, t, sigma)
-% automatically compute the Gaussian kernel by providing the dimension and
-% the standard variance.
-% Derivation:
-% assume multivariables are isometric and independent, which means their
-% covariance matrix is formed as s^2.I. Then the multivariable Gaussian
-% distribution is given with the full equation as:
-% 1/((2pi)^D*det(t^2.*I))^(1/2) exp(-0.5*(x-u)'*1/(t^2)*inv(I)*(x-u)) 
-% simplify this by det(t^2.*I)=(t^2)^D=t^(2D) and u=0, we have
-% 1/((2pi)^D*t^(2D))^(1/2) exp(-0.5*(x)'*1/(t^2)*(x)) = 
-% 1/((2pi)^D*t^(2D))^(1/2) exp(-0.5*1/(t^2).*(x).^2)
-% Author: xiahaa@space.dtu.dk
-    if D == 1
-        x = round(-t*sigma):round(t*sigma);
-        f = @(x) (1/(((2*pi)^D*sigma^(2*D))^(0.5)).*exp((-0.5/(sigma^2)).*(x.^2)));
-        g = f(x);
-    elseif D == 2
-        u = round(-t*sigma):round(t*sigma);
-        [x,y] = meshgrid(u,u);
-        f = @(x,y) (1/(((2*pi)^D*sigma^(2*D))^(0.5)).*exp((-0.5/(sigma^2)).*(x.^2+y.^2)));
-        g = f(x,y);
-    elseif D == 3
-        u = round(-t*sigma):round(t*sigma);
-        [x,y,z] = meshgrid(u,u,u);
-        f = @(x,y,z) (1/(((2*pi)^D*sigma^(2*D))^(0.5)).*exp((-0.5/(sigma^2)).*(x.^2+y.^2+z.^2)));
-        g = f(x,y,z);
-    end
-end
-
 function [flow_u, flow_v] = denseflowLK(im1, im2, hsize)
 % compute optical flow using Lucas-Kanade
     % grad
@@ -117,7 +88,7 @@ function [flow_u, flow_v] = denseflowLK(im1, im2, hsize)
     % dense flow
     width = size(im1,2);
     height = size(im1,1);
-    [yy,xx] = meshgrid(1:width,1:height);
+    [yy,xx] = meshgrid(1:height,1:width);
     xx = vec(xx');
     yy = vec(yy');
     
@@ -135,6 +106,7 @@ function [flow_u, flow_v] = denseflowLK(im1, im2, hsize)
     
     % gaussian filter
     ker = gaussian_kernel_calculator(2, hsize, sigma);
+    ker = ker./sum(ker(:));
     sIx2 = imfilter(Ix2,ker,'replicate','same');
     sIy2 = imfilter(Iy2,ker,'replicate','same');
     sIxy = imfilter(Ixy,ker,'replicate','same');
@@ -182,12 +154,102 @@ function [flow_u, flow_v] = denseflowLK(im1, im2, hsize)
     flow_v = flow_v + dflow_v;
 end
 
-
-function im = imPreprocessing(im)
-    if size(im,3) == 3
-        im = rgb2gray(im);
+function pyr = GaussianPyramid(im, layers)
+    pyr{1} = im;
+    sigma = 1;
+    for i = 2:layers
+        % gaussian filter
+        img = imgaussfilt(pyr{i-1}, sigma);
+        % subsampling
+        pyr{i-1} = imresize(img,0.5,'nearest');
     end
-    im = im2double(im);
-%     ker = fspecial('gaussian',3,1);
-%     im = imfilter(im,ker,'replicate','same');
 end
+
+function [flow_u, flow_v] = denseflowHS(im1, im2, hsize)
+% compute optical flow using Horn-Shunck method
+    % grad
+    sigma = 1;
+%     [Ix, Iy] = grad2(im1,hsize,sigma);
+    [Ix, Iy] = grad1(im1);
+    
+    % dense flow
+    width = size(im1,2);
+    height = size(im1,1);
+    [yy,xx] = meshgrid(1:height,1:width);
+    xx = vec(xx');
+    yy = vec(yy');
+    
+    flow_u = zeros(height, width);
+    flow_v = zeros(height, width);
+    
+    % precomputing
+    Ix2 = Ix.*Ix;
+    Iy2 = Iy.*Iy;
+    Ixy = Ix.*Iy;
+    % filtering
+    % box filter
+%     M = 2*hsize+1;
+%     ker = ones(M,M);
+    
+    % gaussian filter, symmertric, no need to flip
+    ker = gaussian_kernel_calculator(2, hsize, sigma);
+    sIx2 = imfilter(Ix2,ker,'replicate','same');
+    sIy2 = imfilter(Iy2,ker,'replicate','same');
+    sIxy = imfilter(Ixy,ker,'replicate','same');
+    sIxy2 = sIxy.*sIxy;
+
+    % interpolation
+    It = gradIntensity(xx,yy,vec(flow_u),vec(flow_v),im1,im2);
+    It = reshape(It, height, width);
+    
+    Ixt = Ix.*It;
+    Iyt = Iy.*It;
+        
+    sIxt = imfilter(Ixt,ker,'replicate','same');
+    sIyt = imfilter(Iyt,ker,'replicate','same');
+    % LK-flow    
+    % opt1: vectorization, faster
+    % conditioning: check if the smallest eigenvalue is very close to zero.
+    % since it is a 2x2 positive semidefinite matrix, its eigen value has 
+    % a analytical solution and must be a real value greater or equal to 0.
+    eig_smallest = 0.5.*(sIx2 + sIy2 - sqrt((sIx2-sIy2).^2+4.*sIxy2));
+    eig_largest = 0.5.*(sIx2 + sIy2 + sqrt((sIx2-sIy2).^2+4.*sIxy2));
+    ratio = eig_smallest ./ eig_largest;
+    invalid = ratio < 0.001 | (eig_smallest < 1e-6);
+    
+    % second option is to use Harris 
+    % score = sIx2.*sIy2 - sIxy2 - 0.01.*(sIx2+sIy2).^2
+    
+    % third option is to use the hormonic mean
+    % score = (sIx2.*sIy2 - sIxy2)./(sIx2+sIy2)
+
+    % analytical inversion
+    s = 1./(sIx2.*sIy2 - sIxy2);
+    dflow_u = -( sIy2.*sIxt - sIxy.*sIyt).*s;
+    dflow_v = -(-sIxy.*sIxt + sIx2.*sIyt).*s;
+
+    dflow_u(invalid) = 0;
+    dflow_v(invalid) = 0;
+        
+    flow_u = flow_u + dflow_u;
+    flow_v = flow_v + dflow_v;
+    %-------- this ends the initialization, then start HS iteration --------%
+    
+    % kernel 1, hard code 3x3 averaging
+%     ker_avg = [1/12 1/6 1/12;1/6 0 1/6;1/12 1/6 1/12];
+    ker_avg = gaussian_kernel_calculator(2, hsize, sigma);
+    
+    max_iter = 50;
+    alpha = 0.5;
+    for iter = 1:max_iter
+        % arveraging
+        ubar = imfilter(flow_u,ker_avg,'replicate','same');
+        vbar = imfilter(flow_v,ker_avg,'replicate','same');
+        % update
+        den = alpha*alpha + Ix2 + Iy2;
+        flow_u = ubar - (Ix2.*ubar + Ixy.*vbar + Ixt)./den;
+        flow_v = vbar - (Ixy.*ubar + Iy2.*vbar + Iyt)./den;
+    end
+    
+end
+
