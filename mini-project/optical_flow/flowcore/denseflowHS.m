3dcaddad0cca4464d33a2fe554a287beecd206d0
DTU Space drone <xiahaa@space.dtu.dk>
2019-05-07 10:17:30 +0200

    fix hs bug

--- mini-project/optical_flow/flowcore/denseflowHS.m
+++ mini-project/optical_flow/flowcore/denseflowHS.m
@@ -14,6 +14,12 @@ function [flow_u, flow_v] = denseflowHS(im1, im2, iu, iv, verbose)
     else
         flow_v = iv;
     end
+	
+    % kernel 1, hard code 3x3 averaging
+    %     ker_avg = [1/12 1/6 1/12;1/6 0 1/6;1/12 1/6 1/12];
+	sigma = 1;
+    hsize = 2;
+    ker_avg = gauker(hsize, sigma);% hsize + sigma
 
     maxiter = 3;
     for iter = 1:maxiter
@@ -26,30 +32,30 @@ function [flow_u, flow_v] = denseflowHS(im1, im2, iu, iv, verbose)
         Ix2 = Ix.*Ix;
         Iy2 = Iy.*Iy;
         Ixy = Ix.*Iy;    
+
+   	    sIx2 = imfilter(Ix2,ker_avg,'symmetric','same');
+        sIy2 = imfilter(Iy2,ker_avg,'symmetric','same');
+        sIxy = imfilter(Ixy,ker_avg,'symmetric','same');
         Ixt = Ix.*It;
         Iyt = Iy.*It;
-    
-        % kernel 1, hard code 3x3 averaging
-    %     ker_avg = [1/12 1/6 1/12;1/6 0 1/6;1/12 1/6 1/12];
-        sigma = 1;
-        hsize = 2;
-        ker_avg = gauker(2, 1);% hsize + sigma
+        sIxt = imfilter(Ixt,ker_avg,'symmetric','same');
+        sIyt = imfilter(Iyt,ker_avg,'symmetric','same');
         
-        max_iter = 3;
+        max_iter = 10;
         alpha = 1;
         
         uu = zeros(size(im2));
         vv = zeros(size(im2));
         
-        for iter = 1:max_iter
+        for i = 1:max_iter
             % arveraging
             ubar = imfilter(uu,ker_avg,'replicate','same');
             vbar = imfilter(vv,ker_avg,'replicate','same');
             % update
-            den = alpha*alpha + Ix2 + Iy2;
+            den = alpha*alpha + sIx2 + sIy2;
             
-            du = (Ix2.*ubar + Ixy.*vbar + Ixt)./den;
-            dv = (Ixy.*ubar + Iy2.*vbar + Iyt)./den;
+            du = (sIx2.*ubar + sIxy.*vbar + sIxt)./den;
+            dv = (sIxy.*ubar + sIy2.*vbar + sIyt)./den;
             
             uu = ubar - du;
             vv = vbar - dv;
